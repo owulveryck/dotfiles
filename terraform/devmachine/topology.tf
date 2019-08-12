6fb2c52a151ed0e4947d4148697a85a99d482afe
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-08-12 10:37:41 +0200

    chore: just after terraform 012.upgrade

--- terraform/devmachine/topology.tf
+++ terraform/devmachine/topology.tf
@@ -14,11 +14,11 @@ resource "aws_spot_instance_request" "dev-spot" {
 */
 
 resource "aws_instance" "dev" {
-  ami               = "${data.aws_ami.ubuntu.id}"
-  instance_type     = "${var.instance_type}"
-  key_name          = "${var.keyName}"
+  ami               = data.aws_ami.ubuntu.id
+  instance_type     = var.instance_type
+  key_name          = var.keyName
   availability_zone = "us-east-1d"
-  user_data         = "${file("postInstall.sh")}"
+  user_data         = file("postInstall.sh")
 
   tags = {
     Name = "Dev"
@@ -27,20 +27,21 @@ resource "aws_instance" "dev" {
 
 resource "aws_volume_attachment" "ebs_att" {
   device_name  = "/dev/sdh"
-  volume_id    = "${data.aws_ebs_volume.home.id}"
-  instance_id  = "${aws_instance.dev.id}"
+  volume_id    = data.aws_ebs_volume.home.id
+  instance_id  = aws_instance.dev.id
   skip_destroy = true
 }
 
 resource "aws_route53_record" "dev" {
-  zone_id = "${data.aws_route53_zone.myzone.zone_id}"
-  name    = "${local.hostname}"
+  zone_id = data.aws_route53_zone.myzone.zone_id
+  name    = local.hostname
   type    = "A"
   ttl     = "300"
-  records = ["${aws_instance.dev.public_ip}"]
+  records = [aws_instance.dev.public_ip]
 }
 
 resource "aws_network_interface_sg_attachment" "sg_attachment" {
-  security_group_id    = "${data.aws_security_group.ssh.id}"
-  network_interface_id = "${aws_instance.dev.primary_network_interface_id}"
+  security_group_id    = data.aws_security_group.ssh.id
+  network_interface_id = aws_instance.dev.primary_network_interface_id
 }
+
