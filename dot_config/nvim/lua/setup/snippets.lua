22592c2ed73aaeb6592446fe95755aeaaac5f4b7
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2023-03-14 16:02:34 +0100

    feat: add snippets for wtg

--- dot_config/nvim/lua/setup/snippets.lua
+++ dot_config/nvim/lua/setup/snippets.lua
@@ -5,5 +5,5 @@ require("luasnip.loaders.from_snipmate").lazy_load()
 require("luasnip.loaders.from_vscode").lazy_load()
 
 -- Load custom javascript
-require("luasnip.loaders.from_vscode").lazy_load { paths = { "./snippets/cuec4" } }
+require("luasnip.loaders.from_vscode").lazy_load { paths = { "~/.config/nvim/snippets/"} }
 
