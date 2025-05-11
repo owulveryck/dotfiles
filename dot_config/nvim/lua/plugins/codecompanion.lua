9103f64d5d90aaf90d1067e2b1b1ff9c60ff64c5
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2025-05-11 15:40:30 +0200

    update config

--- dot_config/nvim/lua/plugins/codecompanion.lua
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -1,50 +1,50 @@
 return {
-    {
-        "olimorris/codecompanion.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-        },
-        lazy = false,
-        config = function()
-            require("codecompanion").setup({
-                adapters = {
-                    litellm = function()
-                        return require("codecompanion.adapters").extend("openai", {
-                            name = "litellm", -- Give this adapter a different name to differentiate it from the default ollama adapter
-                            url = "http://localhost:4000/v1/chat/completions",
-                            env = {
-                                api_key = "sk-1234",
-                            },
-                            num_ctx = {
-                                default = 4096,
-                            },
-                            schema = {
-                                model = {
-                                    default = "gemini-1.5-pro",
-                                },
-                            },
-                        })
-                    end,
-                },
-                strategies = {
-                    -- Change the default chat adapter
-                    chat = {
-                        adapter = "litellm",
-                    },
-                    inline = {
-                        adapter = "litellm",
-                    },
-                    agent = {
-                        adapter = "litellm",
-                    },
-                },
-                opts = {
-                    -- Set debug logging
-                    log_level = "DEBUG",
-                },
-            })
-        end,
-        init = function() end,
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
     },
+    lazy = false,
+    config = function()
+      require("codecompanion").setup({
+        adapters = {
+          litellm = function()
+            return require("codecompanion.adapters").extend("openai", {
+              name = "litellm", -- Give this adapter a different name to differentiate it from the default ollama adapter
+              url = "http://localhost:4000/v1/chat/completions",
+              env = {
+                api_key = "sk-1234",
+              },
+              num_ctx = {
+                default = 4096,
+              },
+              schema = {
+                model = {
+                  default = "gemini-2.0-flash",
+                },
+              },
+            })
+          end,
+        },
+        strategies = {
+          -- Change the default chat adapter
+          chat = {
+            adapter = "litellm",
+          },
+          inline = {
+            adapter = "litellm",
+          },
+          agent = {
+            adapter = "litellm",
+          },
+        },
+        opts = {
+          -- Set debug logging
+          log_level = "DEBUG",
+        },
+      })
+    end,
+    init = function() end,
+  },
 }
